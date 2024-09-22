cc253ff717de4beac98cca2175301fbe5b8f4ef7
HaritsN <haritsnuragapadika@mail.ugm.ac.id>
2024-09-23 01:09:26 +0700

    Acara 5

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Geolocation } from '@capacitor/geolocation';
 import Map from '@arcgis/core/Map';
 import MapView from '@arcgis/core/views/MapView';
 import Graphic from '@arcgis/core/Graphic';
 import Point from '@arcgis/core/geometry/Point';
 import SimpleMarkerSymbol from '@arcgis/core/symbols/SimpleMarkerSymbol';
+import ImageryLayer from '@arcgis/core/layers/ImageryLayer';
 
 @Component({
   selector: 'app-home',
@@ -12,70 +12,89 @@ import SimpleMarkerSymbol from '@arcgis/core/symbols/SimpleMarkerSymbol';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
-  private view!: MapView; // Menggunakan non-null assertion
+  mapView: MapView | any;
+  userLocationGraphic : Graphic | any;
+  selectedBasemap: string = "topo-vector";
 
-  constructor() {}
+  constructor () {}
 
-  public async ngOnInit() {
-    // Inisialisasi peta
-    const map = new Map({
-      basemap: "topo-vector"
-    });
-
-    // Mendapatkan lokasi pengguna menggunakan Geolocation
-    const coordinates = await this.getCurrentPosition();
-
-    // Buat instance MapView dengan peta dan lokasi pengguna
-    this.view = new MapView({
-      container: "container",
-      map: map,
-      zoom: 14, // Ubah zoom sesuai kebutuhan
-      center: [coordinates.longitude, coordinates.latitude] // Gunakan lokasi pengguna
-    });
-
-    // Tambahkan marker ke lokasi pengguna
-    this.addMarker(coordinates.latitude, coordinates.longitude);
+  async ngOnInit() {
+    this.initializeMap();
   }
 
-  // Fungsi untuk mendapatkan lokasi pengguna
-  private async getCurrentPosition(): Promise<{ latitude: number; longitude: number }> {
-    try {
-      const position = await Geolocation.getCurrentPosition();
-      return {
-        latitude: position.coords.latitude,
-        longitude: position.coords.longitude
-      };
-    } catch (error) {
-      console.error('Error getting location', error);
-      // Kembalikan nilai default jika terjadi kesalahan
-      return {
-        latitude: 46.2773790378953, // Koordinat default
-        longitude: 74.4313477254261 // Koordinat default
-      };
+  async initializeMap() {
+    const map = new Map({
+      basemap: this.selectedBasemap
+    });
+
+    this.mapView = new MapView({
+      container: "container",
+      map: map,
+      zoom: 6, // Zoom level adjusted for better view of Kansas
+      center: [-97.5, 39.0] // Center map over Kansas
+    });
+
+    let weatherServiceFL = new ImageryLayer({ url: WeatherServiceURL });
+    map.add(weatherServiceFL);
+
+    this.addWeatherPointMarker();
+
+    setInterval(this.updateUserLocationOnMap.bind(this), 10000);
+  }
+
+  async changeBasemap() {
+    if (this.mapView) {
+      this.mapView.map.basemap = this.selectedBasemap;
     }
   }
 
-  // Fungsi untuk menambahkan marker ke peta
-  private addMarker(latitude: number, longitude: number) {
-    const point = new Point({
-      longitude: longitude,
-      latitude: latitude
+  addWeatherPointMarker() {
+    // Create a point in Kansas, USA
+    let point = new Point({
+      longitude: -96.5, // Longitude for Kansas
+      latitude: 38.0   // Latitude for Kansas
     });
 
-    const markerSymbol = new SimpleMarkerSymbol({
-      color: [255, 0, 0],  // Warna marker
+    // Create a symbol for the point
+    let markerSymbol = new SimpleMarkerSymbol({
+      color: [255, 0, 0], // Red color
+      size: '12px', // Size of the marker
       outline: {
-        color: [255, 255, 255],  // Warna outline marker
+        color: [255, 255, 255], // White outline
         width: 2
       }
     });
 
-    const pointGraphic = new Graphic({
+    // Create a graphic and add it to the mapView
+    let pointGraphic = new Graphic({
       geometry: point,
       symbol: markerSymbol
     });
 
-    // Tambahkan graphic marker ke peta
-    this.view.graphics.add(pointGraphic);
+    this.mapView.graphics.add(pointGraphic);
+  }
+
+  async getLocationService(): Promise<number[]> {
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition((resp) => {
+        resolve([resp.coords.latitude, resp.coords.longitude]);
+      });
+    });
+  }
+
+  async updateUserLocationOnMap() {
+    let latLng = await this.getLocationService();
+    let geom = new Point({ latitude: latLng[0], longitude: latLng[1] });
+    if (this.userLocationGraphic) {
+      this.userLocationGraphic.geometry = geom;
+    } else {
+      this.userLocationGraphic = new Graphic({
+          symbol: new SimpleMarkerSymbol(),
+          geometry: geom,
+      });
+      this.mapView.graphics.add(this.userLocationGraphic);
+    }
   }
 }
+
+const WeatherServiceURL = "https://mapservices.weather.noaa.gov/eventdriven/rest/services/radar/radar_base_reflectivity_time/ImageServer";
